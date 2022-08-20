55ffdaf9a9cd38307447d7828fdc8f796dfe274e
RI-CD <82904241+RI-CD@users.noreply.github.com>
2022-08-20 13:02:18 +0530

    Delete article-preview-component-master directory

--- article-preview-component-master/app.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const share=document.querySelector(".share-btn");
-const linkss=document.querySelector(".share");
-console.log("hey");
-share.addEventListener("click",()=>{
-linkss.classList.toggle("hidden");
-console.log("clicked");
-})
